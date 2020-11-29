da08a1da99a6c46a98c35cecf0f7d4462c9ab191
athawley <58050814+athawley@users.noreply.github.com>
2020-11-30 09:45:07 +1300

    Update higherlowergame.py
    Solution

--- higherlowergame.py
+++ higherlowergame.py
@@ -15,31 +15,44 @@
 # 2) Make improvements to the program to make it more robust and efficient.
 
 # Import random number generator
-
+import random
 # Set lives to 5
+lives = 5
 
 # Set score to 0
+score = 0
 
 # Generate random number for user to guess (answer)
+answer = random.randint(1, 10)
 
 # While lives > 0 keep playing
+while lives > 0:
+    # Get user guess
+    user_guess = int(
+        input("Please enter a number between 1 and 10 inclusive: "))
+    # if user guess == answer
+    if user_guess == answer:
+        print("Correct, lte's go again")
+        # Update Score
+        score = score + 1
 
-   # Get user guess
-   user_guess = int(input("Please enter a number between 1 and 10 inclusive: "))
-   # if user guess == answer
+        # Update lives
+        lives = lives + 5
+        answer = random.randint(1, 10)
 
-      # Update Score
-
-      # Update lives
-      
-   # else if user guess < answer
-
-      # too low
-      # lose a life and display message
-   # else if user guess > answer
-
-      # too high
-      # lose a life and display message
+    # else if user guess < answer
+    elif user_guess < answer:
+        # too low
+        print("Your guess was too low.")
+        # lose a life and display message
+        lives = lives - 1
+    # else if user guess > answer
+    elif user_guess > answer:
+        # too high
+        print("Your guess was too high.")
+        # lose a life and display message
+        lives = lives - 1
 
 # when out of lives (end of while loop) display game over message
 # display final score
+print("Game over, you scored " + str(score)
